713b79a876c4c6e736114d8ef795cc7cd9ec494e
tockorea <lim.chae.eun@gmail.com>
2018-04-07 14:15:55 +0900

    Lesson16 - Refs

---
+++ lesson16/app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    output: 'Your fav food'
+  },
+  methods: {
+    readRefs: function() {
+      console.log(this.$refs);
+      this.output = this.$refs.input.value;
+    }
+  }
+});
