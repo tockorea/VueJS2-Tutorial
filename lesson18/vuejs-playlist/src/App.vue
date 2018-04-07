bcfe4a3fb72c58a5140043d1bc09fc3a3b0383f7
tockorea <lim.chae.eun@gmail.com>
2018-04-07 15:29:13 +0900

    Lesson18 - Vue Files & The Root Component

---
+++ lesson18/vuejs-playlist/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Your first Vue file, wooo!"
+    };
+  },
+  methods: {
+    greeting: function() {
+      return "heeeey cowboy!";
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
