9aef0b18c77c6b78151202f07419580dbf1050fd
tockorea <lim.chae.eun@gmail.com>
2018-04-07 21:43:19 +0900

    Lesson20 - Component CSS (scoped)

---
+++ lesson20/vuejs-playlist/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+import Ninjas from "./Ninjas";
+
+export default {
+  components: {
+    ninjas: Ninjas
+  },
+  data() {
+    return {
+      title: "Ninja App"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: purple;
+}
+</style>
