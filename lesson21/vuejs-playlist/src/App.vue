6245b00cbeab6d5713fc6f46e150116313718d54
tockorea <lim.chae.eun@gmail.com>
2018-04-07 22:31:21 +0900

    Lesson21 - Nesting Components Examples

---
+++ lesson21/vuejs-playlist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Ninjas from "./components/Ninjas";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninjas": Ninjas
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+
+</style>
