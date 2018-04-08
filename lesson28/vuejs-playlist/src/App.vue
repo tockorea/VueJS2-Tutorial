e6f411c1cac7a319df86884693d87da511fe87bb
tockorea <lim.chae.eun@gmail.com>
2018-04-08 19:34:31 +0900

    Lesson28 - Dynamic Components

---
+++ lesson28/vuejs-playlist/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component='form-one'">Show form one</button>
+    <button v-on:click="component='form-two'">Show form two</button>
+  </div>
+</template>
+
+<script>
+import formOne from "./components/formOne";
+import formTwo from "./components/formTwo";
+
+export default {
+  components: {
+    "form-one": formOne,
+    "form-two": formTwo
+  },
+  data() {
+    return {
+      component: "form-two"
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
