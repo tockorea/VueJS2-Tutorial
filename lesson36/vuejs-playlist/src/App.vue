1863d68d5a897349406714f672dab1a47930929b
tockorea <lim.chae.eun@gmail.com>
2018-04-09 23:41:05 +0900

    Lesson36 - Custom Search Filter

---
+++ lesson36/vuejs-playlist/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<script>
+import addBlog from "./components/addBlog";
+import showBlogs from "./components/showBlogs";
+
+export default {
+  components: {
+    "add-blog": addBlog,
+    "show-blogs": showBlogs
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
