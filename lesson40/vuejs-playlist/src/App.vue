997de5d1cd13f209f23edbbf7af7bfb0a9ba81c5
tockorea <lim.chae.eun@gmail.com>
2018-04-10 23:42:37 +0900

    Lesson40 - Hash vs History (Routing)

---
+++ lesson40/vuejs-playlist/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import addBlog from "./components/addBlog";
+import showBlogs from "./components/showBlogs";
+import listBlogs from "./components/listBlogs";
+
+export default {
+  components: {
+    "add-blog": addBlog,
+    "show-blogs": showBlogs,
+    "list-blogs": listBlogs
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
