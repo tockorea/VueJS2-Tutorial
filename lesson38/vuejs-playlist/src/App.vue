09f88d346e105ba6bf9aedd37b2503af54b7e137
tockorea <lim.chae.eun@gmail.com>
2018-04-10 22:54:19 +0900

    Lesson38 - Mixins

---
+++ lesson38/vuejs-playlist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <show-blogs></show-blogs>
+    <list-blogs></list-blogs>
+  </div>
+</template>
+
+<script>
+import addBlog from "./components/addBlog";
+import showBlogs from "./components/showBlogs";
+import listBlogs from "./components/listBlogs";
+
+export default {
+  components: {
+    "add-blog": addBlog,
+    "show-blogs": showBlogs,
+    "list-blogs": listBlogs
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
