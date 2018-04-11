ec763841423394d66cb9700b5d92a322f015c0b9
tockorea <lim.chae.eun@gmail.com>
2018-04-11 22:43:17 +0900

    Lesson42 - Route Parameters

---
+++ lesson42/vuejs-playlist/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import addBlog from "./components/addBlog";
+import showBlogs from "./components/showBlogs";
+import listBlogs from "./components/listBlogs";
+import header from "./components/header";
+
+export default {
+  components: {
+    "add-blog": addBlog,
+    "show-blogs": showBlogs,
+    "list-blogs": listBlogs,
+    "app-header": header
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
---
+++ lesson42/vuejs-playlist/src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
+
+// Use vue-resource package
+Vue.use(VueResource);
+// Use vue-router package
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+})
+
+// Custom directives
+Vue.directive('theme', {
+  bind(el, binding, vnode) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = '1200px';
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = '560px';
+    }
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+});
+
+// Filters
+Vue.filter('snippet', function(value) {
+  return value.slice(0, 100) + '...';
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router: router
+})
